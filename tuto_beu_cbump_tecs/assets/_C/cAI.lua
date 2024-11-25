1daf7cc64a48fc7398059103609570a48f178f9c
mokalux <mouradlux@gmail.com>
2024-11-25 02:58:18 +0100

    redo

--- tuto_beu_cbump_tecs/assets/_C/cAI.lua
+++
@@ -1,7 +0,0 @@
-CAI = Core.class()
-
-function CAI:init(xstartpos, dx, dy)
-	self.startpos = xstartpos
-	self.dx = dx -- delta x
-	self.dy = dy -- delta y
-end
--- tuto_beu_cbump_tecs/assets/_C/cCollisionBox.lua
+++
@@ -1,6 +0,0 @@
-CCollisionBox = Core.class()
-
-function CCollisionBox:init(xcollwidth, xcollheight)
-	self.w = xcollwidth
-	self.h = xcollheight
-end
